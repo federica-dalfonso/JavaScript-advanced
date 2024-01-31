eaa91ccdcb8ae8ff9a2306baa41a674019993f31
Federica D'Alfonso <federica.dalfonso@gmail.com>
2024-01-31 17:30:26 +0100

    esercizi + extra

---
+++ esercizi.js
@@ -0,0 +1,88 @@
+//1- Funzione che controlli due numeri interi. Ritorna "true" se uno dei due è 50 o se la somma dei due è 50.
+function checkNumber (a, b) {
+    if (a === 50 || b === 50|| a + b === 50) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/*2- Funzione che rimuova il carattere ad una specifica posizione da una stringa. Passa la stringa e la 
+posizione come parametri e ritorna la stringa modificata.*/
+function modifyString (string, [index]) {
+    if (typeof string === string) {
+        let newString = string.replace(string.at([index]), "");
+        return newString;
+    } else {
+        return "Non hai inserito una stringa!"
+    }
+}
+
+/*3- Funzione che controlli se due numeri sono compresi tra 40 e 60 o tra 70 e 100. Ritorna "true" se rispecchiano
+queste condizioni, altrimenti "false"*/
+function includedNumber (a, b){
+    if (((40 < a && a < 60) || (40 < b && b < 60)) || ((70 < a && a < 100) || (70 < b && b < 100))) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/*4- Funzione che accetta un nome di città come parametro e ritorna il nome stesso se inizia con "Los" o "New",
+altrimenti ritorni "false" */
+function cityMatch (string) {
+    if ((string.startsWith("Los") || string.startsWith("New"))) {
+        return string;
+    } else {
+        return false;
+    }
+}
+
+//5- Funzione che calcoli e ritorni la somma di tutti gli elementi di un array. L'array deve essere passato come parametro */
+function addArray (array) {
+    let sum = 0; 
+    for ( let i = 0; i < array.length; i++ ) {
+        sum += array[i];
+    }
+    return sum;
+}
+
+//6- Funzione che controlli che un array NON contenga i numeri 1 o 3. Se NON li contiene, ritorna "true" altrimenrti "false.
+function findNum (array) {
+    if (array.includes(1) || array.includes(3)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+/*7 - Funzione per trovare il tipo di angolo i cui gradi sono passati come parametro. VEDI */
+function cornerType (deg) {
+   if (deg < 90) {
+    return "acuto";
+   } else if (deg > 90 && deg < 180) {
+    return "ottuso";
+   } else if (deg === 90) {
+    return "retto";
+   } else if (deg === 180) {
+    return "piatto"
+   } else {
+    return "Non conosco il tuo angolo"
+   }
+}
+
+/*8- Funzione che crei acronimo a partire da una frase. Es. "Fabbrica Italiana Automobili Torino" deve ritornare FIAT */
+function acronym (string) {
+    if (typeof string) {
+        let words = string.split(" ");
+        let firstLetter = [];
+        for (el of words) {
+            let firstOf = el[0];
+            firstLetter.push(firstOf);
+                       
+        }
+        return firstLetter.join("").toUpperCase();
+    } else {
+        return "Non hai inserito una stringa!"
+    } 
+}
---
+++ extra.js
@@ -0,0 +1,94 @@
+//1- Partendo da una stringa (parametro) ritorna il carattere più usato nella stringa stessa.
+function letterRecurrence (string) {
+    let letters = string.split("");
+    let counter = {};
+    let maxLetter = "";
+    let maxCount = 0;
+    
+    for(let i = 0; i < letters.length; i++) {
+        let letter = letters[i];
+        counter[letter] = (counter[letter] || 0) + 1;
+        if(counter[letter] > maxCount) {
+            maxCount = counter[letter];
+            maxLetter = letter;
+        }
+    }
+    return maxLetter;
+}
+
+/*2- Controlla che due stringhe passate come parametri siano gli anagrammi l’una dell’altra. 
+Ignora punteggiatura e spazi e ricordate di rendere la stringa tutta in minuscolo. Se le due parole sono anagrammi, 
+ritorna `true`, altrimenti ritorna `false`.*/ 
+function isAnagram (string1, string2) {
+    let firstWord = string1.toLowerCase();
+    let secondWord = string2.toLowerCase();
+    firstWord = firstWord.replace(/[^\w]|_/g, "").split("").sort().join("");
+    secondWord= secondWord.replace(/[^\w]|_/g, "").split("").sort().join("");
+    if (firstWord === secondWord) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/*3- Partendo da una lista di possibili anagrammi e da una parola (entrambi passati come parametri), ritorna un nuovo array 
+contenente tutti gli anagrammi corretti della parola data.*/
+function wichAnagram (string, array) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        if (isAnagram(string, array[i])) { //richiamo la funzione dell'es.2
+            result.push(array[i]);
+        }
+    }
+    return result
+}
+
+//4- Partendo da una stringa passata come parametro, ritorna `true` se la stringa è palindroma o `false` se non lo è.
+function palindrome (string) {
+    let stringToAnalize = string.toLowerCase();
+    let stringToCompare = stringToAnalize.split("").reverse().join("");
+    if (stringToCompare === stringToAnalize) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//5- Partendo da un numero intero (dai parametri) ritorna un numero che contenga le stesse cifre, ma in ordine contrario.
+function reverseNumber (x) {
+    let reverse = x.toString().split("").reverse().join("");
+    return reverse;
+}
+
+/*6 - Scrivi una funzione che accetti un numero positivo X come parametro. La funzione dovrebbe stampare a console una “scala” 
+creata con il carattere “#” e avente X scalini.*/
+function hashtagStairs (y) {
+    for (let i = 1; i <= y; i++) {
+        console.log("#".repeat(i))
+    }
+    return "";
+}
+
+//7- Crea una funzione che, data una stringa come parametro, ritorni la stessa stringa, ma al contrario.
+function reverse (string) {
+    let rev = string.split("").reverse().join("");
+    return rev 
+}
+
+//8 - Crea una funzione che accetti un array e un numero Y come parametro. Dividi l’array in sotto-array aventi lunghezza Y.
+function arraySplit (array, z) {
+    let subArray = []; 
+    for (el of array) {
+        subArray.push(array.slice(el, el + z))
+    } 
+    return subArray
+}
+
+/*9- Scrivi una funzione che accetti un numero positivo X come parametro. La funzione dovrebbe stampare a console
+una “piramide” creata con il carattere “#” e avente X strati.*/
+function hashtagPiramid (v) {
+    for (let i = 1; i <= v; i++) {
+        console.log("#".repeat(i,v));
+    }
+    return "";
+}
